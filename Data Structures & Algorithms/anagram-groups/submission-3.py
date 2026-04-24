a37bbf58c7f1e165c074b5b6231e68b917abf241
jimmymani79 <167142865+jimmymani79@users.noreply.github.com>
2026-04-24 16:17:27 -0700

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.py
@@ -0,0 +1,13 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # sort each and store in a dictionary with sorted: [original]
+        # so if next string(sorted) is in keys , attach to the values
+        # iterate through the values and return result.
+
+        visited = {}
+        for s in strs:
+            sort_s = "".join(sorted(s))
+            val = visited.get(sort_s, [])
+            val.append(s)
+            visited[sort_s] = val
+        return list(visited.values())
